3d07111b336f38288c7d3e069de1ff7a94754fec
haleykelling <haley@kellings.com>
2020-09-04 11:03:05 -0600

    Add prop type test for input component.

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Input from './Input';
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, checkProps } from '../test/testUtils';
 
-const setup = (props={}) => {
-    return shallow(<Input {...props}/>)
+const defaultProps = {secretWord: 'party'}
+
+const setup = (props) => {
+    const setupProps = {...defaultProps, ...props}
+    return shallow(<Input {...setupProps}/>)
 }
 
 test('renders component without error', () => {
     const wrapper = setup()
     const component = findByTestAttr(wrapper, 'component-input')
     expect(component.length).toBe(1)
-})
+});
+test('does not throw warning with expected props', () => {
+    checkProps(Input, defaultProps)
+});
